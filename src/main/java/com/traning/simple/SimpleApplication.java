6531c782ad28264366132818d7305a1808aad3a2
Ashwani saxena <sme.ashwani@gmail.com>
2022-05-08 01:04:02 +0530

    IOC

--- src/main/java/com/traning/simple/SimpleApplication.java
+++ src/main/java/com/traning/simple/SimpleApplication.java
@@ -1,14 +1,28 @@
 package com.traning.simple;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@PropertySource("classpath:application.properties")
 public class SimpleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleApplication.class, args);
-		System.out.println("Welcome to spring");
-	}
+		//ApplicationContext app = SpringApplication.run(SimpleApplication.class, args);
+		AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(SimpleApplication.class);
+		Employee bean = app.getBean(Employee.class);
+		System.out.println(bean);
+		System.out.println(bean.getAddress());
+		System.out.println(bean.getEmpDepartment());
 
+		Employee bean1 = app.getBean(Employee.class);
+		System.out.println(bean1);
+		System.out.println(bean1.getAddress());
+		System.out.println(bean1.getEmpDepartment());
+	}
 }
+// Autowiring by type from bean name 'employee' via constructor to bean named 'address'
